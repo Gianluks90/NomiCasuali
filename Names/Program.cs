ed0fb3ad368d04274e0ec99ebdf3d669ad25afd3
Gianluca Marino <45163497+Gianluks90@users.noreply.github.com>
2018-11-20 19:39:35 +0100

    Add files via upload

---
+++ Names/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] names = new string[] {"Gianluca", "Nicoletta", "Andrea", "Luca", "Federico", "Fabio", "Matteo", "Alessio", "Stefano", "Cristian","Valerio","Francesco"};
+            String[] empty = new string[] {"","","","","","","","","","","",""};
+
+            Random random = new Random();
+
+            /*for(int i=0; i<names.Length; ++i) {
+                Console.WriteLine(names);
+            } */
+        int flag = 0;
+        int rand;
+            while (flag < names.Length) {
+                rand = random.Next(0,11);
+                if(empty[flag] == "") {
+                    empty[flag]=names[rand];
+                    ++flag;
+                } 
+            }
+            for(int i=0; i<names.Length; ++i) {
+                Console.Write(empty[i]+" e "+empty[i+1]);
+                ++i;
+            }  
+        }
+    }
+}
